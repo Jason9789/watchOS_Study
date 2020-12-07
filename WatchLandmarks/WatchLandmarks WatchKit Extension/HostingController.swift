f0123ca2e485145ab438ea130fbbb6553a6354d4
Jason9789 <lifebook0809@gmail.com>
2020-12-07 22:37:10 +0900

    [ADD] first project

---
+++ WatchLandmarks/WatchLandmarks WatchKit Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by 전판근 on 2020/12/07.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
